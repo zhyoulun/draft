c5df58f26ebb2f0817ddf387aeb329e2eac2f76d
zhangyoulun <zhangyoulun@360.cn>
2017-06-30 18:46:00 +0800

    btree

--- code/c/algorithm/001_BTree/BTree.cpp
+++ code/c/algorithm/001_BTree/BTree.cpp
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
-void main(void)
+struct BTree_Node{
+	int n;
+	int *key;
+	int leaf;
+	BTree *c;
+};
+
+typedef struct BTree_Node *BTree;
+
+int BTree_Search(BTree x, int k, BTree x_out)
 {
-	printf("hello");
+	int i=0;
+	while(i < x->n && k > x->key[i]) i++;
+	if(i < x->n && k == x->key[i]){
+		x_out = x;
+		return i;
+	}else if(x->leaf==1){
+		x_out = NULL;
+		return 0;
+	} else{
+	}
+}
+
+int main()
+{
+	printf("hello\n");
+	return 0;
 }
