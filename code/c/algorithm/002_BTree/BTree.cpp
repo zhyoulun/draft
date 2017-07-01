ea8c95137327ecc4fa302517ab3f08b9552f4596
zhangyoulun <154422843@qq.com>
2017-07-01 20:31:30 +0800

    debug

--- code/c/algorithm/002_BTree/BTree.cpp
+++ code/c/algorithm/002_BTree/BTree.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define t 2
+
 typedef struct BTree_Node *BTree;
 
 struct BTree_Node {
@@ -15,6 +17,34 @@ void BTree_PrintCore(BTree T, int depth);
 void BTree_Search(BTree T, int k, BTree *pT_out, int *pi_out);
 BTree BTree_Sample();
 BTree BTree_SampleRandom(int n, int depth);
+void BTree_Delete(BTree T);
+BTree BTree_CreateEmpty();
+void BTree_SplitChild(BTree x, int i);
+BTree BTree_AlocateNode();
+int BTree_CheckNode(BTree T);
+
+BTree BTree_AlocateNode()
+{
+	BTree T = (BTree)malloc(sizeof(struct BTree_Node));
+	//printf("t=%d\n",t);
+	T->key = (int *)malloc((2 * t - 1)*sizeof(int));//键
+	T->c = (BTree *)malloc((2 * t)*sizeof(BTree));//指针
+	T->leaf = 1;//是否为叶子结点
+	T->n = 0;//当前键的个数
+	return T;
+}
+
+int BTree_CheckNode(BTree T)
+{
+}
+
+BTree BTree_CreateEmpty()
+{
+	BTree T = BTree_AlocateNode();
+	T->leaf = 1;
+	T->n = 0;
+	return T;
+}
 
 void BTree_Search(BTree T, int k, BTree *pT_out, int *pi_out)
 {
@@ -28,6 +58,7 @@ void BTree_Search(BTree T, int k, BTree *pT_out, int *pi_out)
 		return;
 	}
 	else if (T->leaf == 1) {
+		free(pT_out);
 		pT_out = NULL;
 		*pi_out = -1;
 	}
@@ -55,6 +86,10 @@ void BTree_PrintCore(BTree T, int depth)
 	}
 	printf("]\n");
 
+	if (T->n == 0) {
+		return;
+	}
+
 	for (i = 0; i < T->n + 1; i++) {
 		for (j = 0; j < depth; j++) {
 			printf(" ");
@@ -72,21 +107,19 @@ BTree BTree_Sample()
 {
 	int i, j;
 
-	BTree T = (BTree)malloc(sizeof(struct BTree_Node));
-	T->n = 2;
+	BTree T = BTree_AlocateNode();
+	T->n = 4;
 	T->leaf = 0;
-	T->key = (int *)malloc(T->n * sizeof(int));
 	for (i = 0; i < T->n; i++) {
 		T->key[i] = 100 * (i + 1);
 	}
-	T->c = (BTree *)malloc((1 + T->n) * sizeof(BTree));
 	for (i = 0; i < 1 + T->n; i++) {
-		BTree tempT = (BTree)malloc(sizeof(struct BTree_Node));
-		tempT->n = 2;
+		BTree tempT = BTree_AlocateNode();
+		tempT->n = 4;
 		tempT->leaf = 1;
 		tempT->key = (int *)malloc(tempT->n * sizeof(int));
 		for (j = 0; j < tempT->n; j++) {
-			tempT->key[j] = 100 * i + (j + 1) * 45;
+			tempT->key[j] = 100 * i + (j + 1) * 25;
 		}
 		tempT->c = (BTree *)malloc((1 + tempT->n) * sizeof(BTree));
 		for (j = 0; j < 1 + tempT->n; j++) {
@@ -103,11 +136,60 @@ BTree BTree_SampleRandom(int n, int depth)
 	return T;
 }
 
+void BTree_Delete(BTree T)
+{
+	if (T == NULL) {
+		return;
+	}
+
+	int i;
+	for (i = 0; i < 1+T->n; i++) {
+		BTree_Delete(T->c[i]);
+	}
+	free(T->key);
+	free(T);
+}
+
+void BTree_SplitChild(BTree x, int i)
+{
+	int j;
+
+	BTree z = BTree_AlocateNode();
+	BTree y = x->c[i];
+	
+	//将y的后半部分复制给z
+	z->leaf = y->leaf;
+	z->n = t - 1;
+	for (j = 0; j < t - 1; j++) {
+		z->key[j] = y->key[t+j];
+	}
+	if (!y->leaf) {
+		for (j = 0; j < t; j++) {
+			z->c[j] = y->c[t+j];
+		}
+	}
+
+	for (j = x->n; j >= i + 1; j--) {
+		x->c[j + 1] = x->c[j];
+	}
+	x->c[i + 1] = z;
+	for (j = x->n - 1; j >= i; j--) {
+		x->key[j + 1] = x->key[j];
+	}
+
+	x->key[i] = y->key[t];
+	x->n = x->n + 1;
+}
+
 int main()
 {
 	BTree T = BTree_Sample();
 
 	BTree_Print(T);
+	//printf("%d\n", T->c[0]->key[1]);
+	//BTree_SplitChild(T->c[1], 1);
+
+	return 0;
 
 	BTree *pT_out = (BTree *)malloc(sizeof(BTree));
 	int *pi_out = (int *)malloc(sizeof(int));
@@ -117,6 +199,16 @@ int main()
 		BTree_Print(*pT_out);
 		printf("i_out=%d\n",*pi_out);
 	}
+
+
+
+
+
+	free(T);
+	free(pT_out);
+
+	T = BTree_CreateEmpty();
+	BTree_Print(T);
 
 	return 0;
 }
